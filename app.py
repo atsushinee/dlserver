69942aeb70d77b5f10f851fcdd2f5f3170998ae7
ihsusta <atsushinee@outlook.com>
2018-05-15 18:56:06 +0800

    add upload

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 static = "./static/"
 
 SP = '''/'''
+spe_dir = 'lichun'
+key = 'lc'
 
 
 def isdir(path):
@@ -16,6 +18,10 @@ def pre(arr, s):
     return [s + x + '/' for x in arr]
 
 
+def to_redirect(msg, v=static):
+    return '/show?v=%s&msg=%s' % (v, msg)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return redirect('/show?v=' + static)
@@ -24,8 +30,12 @@ def index():
 @app.route('/show', methods=['GET', 'POST'])
 def show():
     v = request.args.get("v")
+    msg = request.args.get("msg")
+    if v is not None and spe_dir in v and request.args.get('key') != key:
+        return redirect(to_redirect('权限不足!'))
+
     if v is None or not v.startswith(static):
-        return redirect('/show?v=' + static)
+        return redirect(to_redirect("Bad request!"))
     if os.path.isdir(v):
         a = os.listdir(v)
         print(v)
@@ -36,9 +46,26 @@ def show():
             isdir=isdir,
             pre=SP.join(v.split(SP)[0:-2]) + SP,
             haspre=v != static,
-            pwd=SP.join(v.split(SP)[2:]))
+            pwd=SP.join(v.split(SP)[2:]),
+            v=v,
+            msg=msg)
     else:
         return v
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    v = request.args["v"]
+    if request.method == 'POST':
+        f = request.files['file']
+        if f.filename == '':
+            return redirect(to_redirect('文件名不可为空!'))
+        f.save(v + f.filename)
+        if spe_dir in v:
+            return redirect(
+                to_redirect("文件[%s]上传成功!%s" % (f.filename, '&key=%s' % key), v))
+        else:
+            return redirect(to_redirect("文件[%s]上传成功!" % f.filename, v))
 
 
 if __name__ == '__main__':
